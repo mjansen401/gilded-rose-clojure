7a29ec105f7993003e6826421e360d52b7b8c58d
Mike Jansen <mjansen401@gmail.com>
2013-08-23 12:03:51 -0500

    todo: write tests later

---
+++ spec/gilded_rose/core_spec.clj
@@ -0,0 +1,6 @@
+(ns gilded-rose.core-spec
+(:require [speclj.core :refer :all]
+          [gilded-rose.core :refer [update-quality]]))
+
+(describe "gilded rose"
+          (it "TODO - write tests later"))
