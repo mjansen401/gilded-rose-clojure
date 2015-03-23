c2d32d8bc2784bbd2dc6cdf95f1aa43fd42df776
Sarudak <SPaulson@paraport.com>
2015-03-23 15:38:10 -0700

    #PR323 Fixed issue with items going below 0 and over 50

--- project.clj
+++ project.clj
@@ -5,5 +5,4 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.4.0"]]
   :profiles {:dev {:dependencies [[midje "1.6.3"]]}}
-  :plugins [[lein-midje "3.1.3"]]
-  :test-paths ["spec"])
+  :plugins [[lein-midje "3.1.3"]])
