f05c963d28e8840bad5180636770b006cecd8f90
Anders Engström <anders.engstrom@hrmsoftware.se>
2016-01-25 21:12:14 +0100

    Add moar tests

--- spec/gilded_rose/core_spec.clj
+++ spec/gilded_rose/core_spec.clj
@@ -1,6 +1,26 @@
 (ns gilded-rose.core-spec
 (:require [speclj.core :refer :all]
-          [gilded-rose.core :refer [update-quality]]))
+          [gilded-rose.core :refer [update-quality item]]))
 
-(describe "gilded rose"
-          (it "TODO - write tests later"))
+(defn any-item [sell-in quality]
+  (item "+5 Dexterity Vest" sell-in quality))
+
+(defn iterate-updated
+  "Create a lazy iterator that will contain recursive updates for an item"
+  [item]
+  (let [update-fn (fn [i] (first (update-quality [i])))]
+    (iterate update-fn item)))
+
+(defn item-after [item days]
+  (last (take (+ days 1) (iterate-updated item))))
+
+(describe "a generic item"
+          (it "should decrease quality every day"
+              (should-be-same 5
+                              (:quality (item-after (any-item 5 8) 3))))
+          (it "should decrease with 2 pts after expiry"
+              (should-be-same 3
+                              (:quality (item-after (any-item 2 9) 4)))))
+(describe "brie"
+          (it "increases quality the older it gets")
+          (it "can only increase to max 50"))
