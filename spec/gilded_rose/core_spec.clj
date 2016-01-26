5b6b269560786ab1fe5a624b05f6f6439ae52821
Anders Engström <anders.engstrom@hrmsoftware.se>
2016-01-26 08:01:49 +0100

    Refactor to let-expressions

--- spec/gilded_rose/core_spec.clj
+++ spec/gilded_rose/core_spec.clj
@@ -2,8 +2,8 @@
 (:require [speclj.core :refer :all]
           [gilded-rose.core :refer [update-quality item]]))
 
-(defn any-item [sell-in quality]
-  (item "+5 Dexterity Vest" sell-in quality))
+(def any-item (partial item "+5 Dexterity Vest"))
+(def smelly-cheese (partial "Aged Brie"))
 
 (defn iterate-updated
   "Create a lazy iterator that will contain recursive updates for an item"
@@ -11,16 +11,22 @@
   (let [update-fn (fn [i] (first (update-quality [i])))]
     (iterate update-fn item)))
 
-(defn item-after [item days]
+(defn item-after
+  "Get the item after n days"
+  [item days]
   (last (take (+ days 1) (iterate-updated item))))
 
 (describe "a generic item"
           (it "should decrease quality every day"
-              (should-be-same 5
-                              (:quality (item-after (any-item 5 8) 3))))
+              (let [item (item-after (any-item 5 8) 3)]
+                (should-be-same 5 (:quality item))))
           (it "should decrease with 2 pts after expiry"
-              (should-be-same 3
-                              (:quality (item-after (any-item 2 9) 4)))))
+              (let [item (item-after (any-item 2 9) 4)]
+                (should-be-same 3 (:quality item)))))
 (describe "brie"
-          (it "increases quality the older it gets")
-          (it "can only increase to max 50"))
+          (it "increases quality the older it gets"
+              (let [brie (item-after (item "Aged Brie" 5 5) 10)]
+                (should-be-same 15 (:quality brie))))
+          (it "can only increase to max 50"
+              (let [brie (item-after (item "Aged Brie" 5 49) 5)]
+                (should-be-same 50 (:quality brie)))))
